75ee23b8679953390b2d830fae97657eb55d850e
Awab Mohamed <awabmohamed123@gmail.com>
2024-05-08 10:41:19 -0400

    updated api to fetch via https

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=fe06dc4b';
+const API_URL = 'https://www.omdbapi.com?apikey=fe06dc4b';
 
 const App = () => {
     
